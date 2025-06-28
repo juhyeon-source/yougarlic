9939084ff430ad69b40639f36fce3bba4da42fb5
juhyeon <wwg6710@naver.com>
2025-06-28 15:03:20 +0900

    짜잘한 코드 수정

--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import Navbar from '../components/Navbar'; 
+import Navbar from '../components/Navbar';
 
 
 const Board = () => {
